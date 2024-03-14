280a7962b535be23b05b7462bfe8a13388189722
RAAS <acerocol67@gmail.com>
2024-03-13 22:29:00 -0400

    add analytics by vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Ubuntu_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const ubuntuMono = Ubuntu_Mono({ subsets: ["latin"] ,weight:["400","700"], style:"normal",preload:true});
 
 export const metadata = {
@@ -11,6 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <Analytics />
+      <SpeedInsights/>
       <body className={ubuntuMono.className}>{children}</body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -6,9 +6,9 @@ export default async function Home() {
   const file = await fs.readFile(process.cwd() + '/lib/lands.json', 'utf8');
  const lands =  JSON.parse(file);
   return (
-    <main className="flex min-h-screen w-[99%] flex-col items-center justify-center  gap-y-20 px-10 text-black">
+    <main className="flex min-h-screen w-[99%] flex-col items-center justify-center  gap-y-20 px-10 text-black mt-8">
       {lands.map((land)=>{
-        return <div className="w-full rounded-sm flex-col bg-white pt-2 gap-10">
+        return <div key={`land-${land.plot}`} className="w-full rounded-sm flex-col bg-white pt-2 gap-10">
           <LandProperties key={`land-#${land.plot}`} land={land}/>
           <LandFrame key={`frame-#${land.plot}`} number={land.plot} />
           </div>
