af0d50f562f1865db72437b8052cb59104f9f70b
RAAS <acerocol67@gmail.com>
2024-03-16 02:04:26 -0400

    add timer to tree

--- components/landProperties.jsx
+++ components/landProperties.jsx
@@ -1,24 +1,25 @@
 import LandIsLock from "./landIsLock";
-const LandProperties = ({land}) => {
+import LandTreeTimer from "./landTreeTimer";
+const LandProperties = ({ land }) => {
    const industries = {
-      mine:"⛏️",
-      textile:"👗",
-      sluggery:"🐛",
-      kiln:"🧱",
-      apiary:"🐝",
-      woodwork:"🔨"
+      mine: "⛏️",
+      textile: "👗",
+      sluggery: "🐛",
+      kiln: "🧱",
+      apiary: "🐝",
+      woodwork: "🔨"
    }
- return <div className="flex w-full flex-row flex-wrap justify-stretch items-center gap-4 px-3">
-   <span className="font-bold text-2xl">{`#${land.plot}`}</span>
-   <LandIsLock land={land}/>
-   <span className="font-bold text-2xl">{`🌳${land.treeCount}`}</span>
-   <span className="font-bold text-2xl">{`🌱${land.soilCount}`}</span>
-   <span className="font-bold text-2xl">{land.windmill?"🌾":""}</span>
-   <span className="font-bold text-2xl">{land.coop?"🐓":""}</span>
-   {land.industries.map((e,i)=>{
-      return <span key={`indus-${i}`} className="font-bold text-2xl">{industries[e]??e.toUpperCase()}</span>
-   })}
-  </div>
+   return <div className="flex w-full flex-row flex-wrap justify-stretch items-center gap-4 px-3">
+      <span className="font-bold text-2xl">{`#${land.plot}`}</span>
+      <LandIsLock land={land} />
+      <LandTreeTimer land={land} />
+      <span className="font-bold text-2xl">{`🌱${land.soilCount}`}</span>
+      <span className="font-bold text-2xl">{land.windmill ? "🌾" : ""}</span>
+      <span className="font-bold text-2xl">{land.coop ? "🐓" : ""}</span>
+      {land.industries.map((e, i) => {
+         return <span key={`indus-${i}`} className="font-bold text-2xl">{industries[e] ?? e.toUpperCase()}</span>
+      })}
+   </div>
 
 };
 
---
+++ components/landTreeTimer.jsx
@@ -0,0 +1,94 @@
+'use client'
+import { useEffect, useState, useRef, useCallback } from "react";
+
+const LandTreeTimer = ({ land }) => {
+   const [treeTimer, setTreeTimer] = useState(false);
+   const [showMsg, setShowMsg] = useState(false);
+   const intervalRef = useRef(null); // Nueva referencia para almacenar el intervalo
+
+   const startTimer = (time = false) => {
+      let now = time ? time : Date.now();
+      let hour7 = now + ((7 * 60 * 60 * 1000) + (15 * 60 * 1000));
+      let fechaFinal = new Date(hour7);
+      localStorage.setItem(land.plot, JSON.stringify({ ...land, treeTimer: fechaFinal }));
+      setTreeTimer(true);
+      launchIterval();
+   }
+
+   const clearTimer = useCallback(() => {
+      if (treeTimer) {
+         console.log("is stop");
+         localStorage.setItem(land.plot, JSON.stringify({ ...land, treeTimer: false }));
+         setTreeTimer(false);
+         setShowMsg(false);
+         clearInterval(intervalRef.current);
+      }
+   }, [land, treeTimer]);
+
+   const autoResetTimer = useCallback(() => {
+      setTimeout(() => {
+         console.log("is auto reset stop");
+         setShowMsg(false);
+         let metadata = JSON.parse(localStorage.getItem(land.plot));
+         if (metadata?.treeTimer !== false) {
+            let time = new Date(metadata.treeTimer);
+            startTimer(time);
+         } else {
+            startTimer();
+         }
+      }, 10000);
+   }, [land, startTimer]);
+
+   const launchIterval = useCallback(() => {
+      clearInterval(intervalRef.current);
+      intervalRef.current = setInterval(() => {
+         let metadata = JSON.parse(localStorage.getItem(land.plot));
+         if (metadata?.treeTimer && metadata?.treeTimer != false) {
+            let ahora = new Date();
+            let time = new Date(metadata?.treeTimer);
+            let diferencia = time - ahora;
+            if (diferencia <= 0) {
+               clearInterval(intervalRef.current);
+               setShowMsg("AHORA 🪓");
+               autoResetTimer();
+               return;
+            }
+            let horas = Math.floor(diferencia / (1000 * 60 * 60));
+            let minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+            let segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+            let horasFormateadas = horas.toString().padStart(2, '0');
+            let minutosFormateados = minutos.toString().padStart(2, '0');
+            let segundosFormateados = segundos.toString().padStart(2, '0');
+            if (treeTimer) {
+               setShowMsg(`${horasFormateadas}:${minutosFormateados}:${segundosFormateados}`);
+            } else {
+               clearTimer();
+               return;
+            }
+         } else {
+            clearTimer();
+            return;
+         }
+      }, 1000);
+   }, [clearTimer, autoResetTimer, treeTimer]);
+
+
+
+   const handleStartStopTimer = () => {
+      if (treeTimer) {
+         clearTimer();
+      } else {
+         startTimer();
+      }
+   }
+
+
+   useEffect(() => {
+      launchIterval();
+   }, [launchIterval]);
+   return <span onClick={handleStartStopTimer} className="cursor-pointer font-bold text-2xl">{`🌳${land.treeCount} ${treeTimer ? showMsg ? showMsg : "" : ""}`}</span>
+};
+
+export default LandTreeTimer;
+
+
