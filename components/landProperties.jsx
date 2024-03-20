032638644e8e4eb5f39b8f7b8e1f95ac9ec4e5e6
RAAS <acerocol67@gmail.com>
2024-03-20 09:32:29 -0400

    ajustes

--- components/landProperties.jsx
+++ components/landProperties.jsx
@@ -1,17 +1,13 @@
 import LandIsLock from "./landIsLock";
 import LandTreeTimer from "./landTreeTimer";
+import { industries, landsTypes } from "@/lib/commons";
 const LandProperties = ({ land }) => {
-   const industries = {
-      mine: "⛏️",
-      textile: "👗",
-      sluggery: "🐛",
-      kiln: "🧱",
-      apiary: "🐝",
-      woodwork: "🔨",
-      winery: "🍷"
-   }
-   return <div className="flex w-full flex-row flex-wrap justify-stretch items-center gap-4 px-3">
+   return <div className={`flex w-full flex-row flex-wrap justify-stretch items-center gap-4 px-3`}>
       <span className="font-bold text-2xl">{`#${land.plot}`}</span>
+      <span className="p-2 my-1 flex flex-row justify-center items-center gap-4 text-xl uppercase">
+         <img src={landsTypes[land.type].urlIcon} alt={land.type} width="45px" height="45px" />
+         {land.type}
+      </span>
       <LandIsLock land={land} />
       <LandTreeTimer land={land} />
       <span className="font-bold text-2xl">{`🌱${land.soilCount}`}</span>
@@ -21,7 +17,6 @@ const LandProperties = ({ land }) => {
          return <span key={`indus-${i}`} className="font-bold text-2xl">{industries[e] ?? e.toUpperCase()}</span>
       })}
    </div>
-
 };
 
 export default LandProperties;
